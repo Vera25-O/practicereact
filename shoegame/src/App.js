1c33f3d858792ba5a0ccbc3e9046bd05f6f33138
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-07-22 00:54:05 +0300

    created Login.js and Home.js

--- shoegame/src/App.js
+++ shoegame/src/App.js
@@ -1,9 +1,25 @@
-
+import Login from "./components/Login";
 
 function App() {
   return (
     <body className="App">
       <h1>SHOEGAME</h1>
+      <switch>
+      <Login/>
+      </switch>
+
+      <switch>
+      <Home/>
+        </switch>
+
+        <switch>
+        <About/>
+        </switch>
+
+        <switch>
+        <Shoes/>
+        </switch>
+     
     </body>
   );
 }
